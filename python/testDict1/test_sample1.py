f401a57d9f01578dd7e6592a5da5600d6d95305a
润廷 <wangwanzhi.wwz@antgroup.com>
2023-07-31 15:07:10 +0800

    update case

--- python/testDict1/test_sample1.py
+++ python/testDict1/test_sample1.py
@@ -11,22 +11,22 @@ def test_add0():
 
 def test_add1():
     x,y = 100,1
-    num = add0(x,y)
+    num = add1(x,y)
     assert num == 101
 
 def test_add2():
     x,y = 100,1
-    num = add0(x,y)
+    num = add2(x,y)
     assert num == 102
 
 def test_add3():
     x,y = 100,1
-    num = add0(x,y)
+    num = add3(x,y)
     assert num == 103
 
 def test_add4():
     x,y = 100,1
-    num = add0(x,y)
+    num = add4(x,y)
     assert num == 104
 
 
@@ -38,34 +38,34 @@ def test_random0():
         num = sub0(x,y)
     assert num == 100
 
-def test_add1():
+def test_random1():
     x,y = 100,1
     if(y>0):
-        num = add0(x,y)
+        num = add1(x,y)
     else:
-        num = sub0(x,y)
+        num = sub1(x,y)
     assert num == 101
 
-def test_add2():
+def test_random2():
     x,y = 100,-1
     if(y>0):
-        num = add0(x,y)
+        num = add2(x,y)
     else:
-        num = sub0(x,y)
+        num = sub2(x,y)
     assert num == 102
 
-def test_add3():
+def test_random3():
     x,y = 100,1
     if(y>0):
-        num = add0(x,y)
+        num = add3(x,y)
     else:
-        num = sub0(x,y)
+        num = sub4(x,y)
     assert num == 103
 
-def test_add4():
+def test_random4():
     x,y = 100,-1
     if(y>0):
-        num = add0(x,y)
+        num = add4(x,y)
     else:
-        num = sub0(x,y)
-    assert num == 104
+        num = sub4(x,y)
+    assert num == 96
